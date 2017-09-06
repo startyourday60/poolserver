32ae9c3291a899a956d899bb40bdfd1a1973c17f
orignal <i2porignal@yandex.ru>
2017-09-06 11:53:15 -0400

    convert hash to Little Endian

--- src/server/shared/Crypto.cpp
+++ src/server/shared/Crypto.cpp
@@ -5,10 +5,14 @@ namespace Crypto
 {
 	BinaryData GOSTD(BinaryData data)
 	{
-		uint8_t hash1[64];
+		uint8_t hash1[64], hash2[32];
 		i2p::crypto::GOSTR3411_2012_512 (&data[0], data.size (), hash1);	
+		i2p::crypto::GOSTR3411_2012_256 (hash1, 64, hash2);
 		std::vector<byte> hash(32);
-		i2p::crypto::GOSTR3411_2012_256 (hash1, 64, &hash[0]);
-		return 	std::vector<byte>(hash.begin(), hash.end());
+		// To Little Endian. TODO implement faster
+		for (int i = 0; i < 32; i++)
+			hash[i] = hash2[31-i]; 
+		
+		return 	hash;
 	}
 }
