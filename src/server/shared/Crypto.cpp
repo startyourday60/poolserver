80df4b090051727e6eaa987d72c0fe8b3cc012a5
orignal <i2porignal@yandex.ru>
2017-09-06 11:55:16 -0400

    convert to Litte Endian

--- src/server/shared/Crypto.cpp
+++ src/server/shared/Crypto.cpp
@@ -8,11 +8,10 @@ namespace Crypto
 		uint8_t hash1[64], hash2[32];
 		i2p::crypto::GOSTR3411_2012_512 (&data[0], data.size (), hash1);	
 		i2p::crypto::GOSTR3411_2012_256 (hash1, 64, hash2);
-		std::vector<byte> hash(32);
+		std::vector<byte> hash;
 		// To Little Endian. TODO implement faster
 		for (int i = 0; i < 32; i++)
-			hash[i] = hash2[31-i]; 
-		
+			hash.push_back (hash2[31-i]); 
 		return 	hash;
 	}
 }
